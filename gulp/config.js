b11798a81bcd2a975dd1381beeb42589352c85bc
David Taieb <david_taieb@us.ibm.com>
2015-10-07 10:20:18 -0400

    enable configuration of src directory

--- gulp/config.js
+++ gulp/config.js
@@ -1,9 +1,10 @@
 var path = require('path');
 
+var mainConfig = require( path.resolve(process.cwd(), 'config.js' ));
 module.exports = {
     root:       path.resolve(process.cwd()) + path.sep,
     mozaikRoot: path.resolve(path.join(__dirname, '..')) + path.sep,
-    src:        path.resolve(path.join(process.cwd(), 'src')) + path.sep,
+    src:        path.resolve(path.join(process.cwd(), mainConfig.src || 'src')) + path.sep,
     mozaikSrc:  path.resolve(path.join(__dirname, '..', 'src')) + path.sep,
     dest:       path.resolve(path.join(process.cwd(), 'build')) + path.sep
 };
