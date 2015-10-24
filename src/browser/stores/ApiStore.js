cf48ec2443c17ee7321a760a1d98a7c6206730e8
David Taieb <david_taieb@us.ibm.com>
2015-10-23 23:33:19 -0400

    Fixed web socket reconnection issue

--- src/browser/stores/ApiStore.js
+++ src/browser/stores/ApiStore.js
@@ -16,7 +16,17 @@ var ApiStore = Reflux.createStore({
         if (config.useWssConnection === true) {
             proto = 'wss';
         }
-
+        
+        //Setup ws
+        this.setupWs(proto);
+        
+        this.listenTo(ApiActions.get, this.get);
+    },
+    
+    setupWs(proto){
+    	if ( this.ws && ws.readyState === WebSocket.OPEN ){
+    		return console.log("websocket already started");
+    	}
         this.ws = new WebSocket(`${ proto }://${ window.document.location.host }`);
         var ws = this.ws;
         ws.onmessage = event => {
@@ -27,23 +37,39 @@ var ApiStore = Reflux.createStore({
             buffer.forEach(request => {
                 ws.send(JSON.stringify(request));
             });
+            
+   			//register the apis again if retrying the connection
+			if (this.retryConnection){
+				console.log("Reconnecting the APIs");
+ 
+    			for ( var i = 0 ; i < this.wsRetryParams.length; i++ ){
+    				var api = this.wsRetryParams[i];
+    				this.get(api.id, api.params);
+    			};
+    			this.retryConnection = false;
+			}
         };
         
         ws.onclose = function(evt){
     		console.log("WebSocket Mozaik API Store closed");
-			//May be timeout
-			//console.log( JSON.stringify(evt) );
-			console.log("Restarting WebSocket Mozaik API Store...");
-			ws = null;
-			delete this.ws;
-			ws = this.ws = new WebSocket(`${ proto }://${ window.document.location.host }`);
-    	}
-        
-        this.listenTo(ApiActions.get, this.get);
+    		delete this.ws;
+			//May be timeout, retry in 1s
+    		setTimeout( function(){
+    			console.log("Restarting WebSocket Mozaik API Store...");
+    			this.retryConnection = true;
+    			this.setupWs(proto);
+    		}.bind(this), 1000);			
+    	}.bind(this);
     },
-
+    
+    retryConnection: false,		//Indicate whether we are reconnecting
+    wsRetryParams : [], //Remember the api params when reconnecting the websocket
+    
     get(id, params) {
     	var ws = this.ws;
+    	if ( !this.retryConnection ){
+    		this.wsRetryParams.push({id:id,params:params});
+    	}
         if (ws === null || ws.readyState !== WebSocket.OPEN) {
             buffer.push({
                 id:     id,
